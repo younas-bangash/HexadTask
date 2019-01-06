a782cbdc555c0e7b56a3df42740126caf1db14be
Younas Bangash <engr.younasbangash@gmail.com>
2019-01-06 12:32:23 +0400

    Code Refactor

--- app/src/main/java/com/hexad/movietask/di/builder/ActivityBuilderModule.java
+++ app/src/main/java/com/hexad/movietask/di/builder/ActivityBuilderModule.java
@@ -1,12 +1,13 @@
 package com.hexad.movietask.di.builder;
 
+import com.hexad.movietask.di.module.ViewModelModule;
 import com.hexad.movietask.view.activity.FragmentContainerActivity;
 
 import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 
 
-@Module
+@Module(includes = ViewModelModule.class)
 public abstract class ActivityBuilderModule {
 
     @ContributesAndroidInjector(modules = FragmentBuilderModule.class)
